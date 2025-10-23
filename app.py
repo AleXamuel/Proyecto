234b7ea975d1c9fc64f538d9c56d3504b8e1f1b4
Samuel <barretodiazsamuel@gmail.com>
2025-10-23 18:26:00 -0500

    CRUD

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_migrate import Migrate
 from db import db
-from models import (
-    Persona, Usuario, Administrador,
-    Cancion, Vinilo, Playlist,
-    Compra, Resena, ContenidoPlaylist,
-    ContenidoVinilos, CompraVinilo, CompraCancion
-)
+from CRUD.persona import bp_persona
+from CRUD.usuario import bp_usuario
+from CRUD.admin import bp_admin
+from CRUD.cancion import bp_cancion
+from CRUD.vinilo import bp_vinilo
+from CRUD.playlist import bp_playlist
+from CRUD.compra import bp_compra
+from CRUD.resena import bp_resena
+from CRUD.compra_cancion import bp_compra_cancion
+from CRUD.compra_vinilo import bp_compra_vinilo
+from CRUD.contenido_playlist import bp_contenido_playlist
+from CRUD.contenido_vinilo import bp_contenido_vinilo
 
 def create_app():
     app = Flask(__name__)
@@ -16,43 +22,25 @@ def create_app():
     db.init_app(app)
     Migrate(app, db)
 
+    # Registrar todos los blueprints
+    app.register_blueprint(bp_persona)
+    app.register_blueprint(bp_usuario)
+    app.register_blueprint(bp_admin)
+    app.register_blueprint(bp_cancion)
+    app.register_blueprint(bp_vinilo)
+    app.register_blueprint(bp_playlist)
+    app.register_blueprint(bp_compra)
+    app.register_blueprint(bp_resena)
+    app.register_blueprint(bp_compra_cancion)
+    app.register_blueprint(bp_compra_vinilo)
+    app.register_blueprint(bp_contenido_playlist)
+    app.register_blueprint(bp_contenido_vinilo)
     @app.get("/api/health")
     def health():
-        return {"ok": True, "message": "API de Tienda Musical activa 🎵"}
+        return {"ok": True, "message": "API REST en funcionamiento ✅"}
 
-    # Persona
-    # usarios
-    # admin
-    # canciones 
-    # vinilos
-    #playlist
-
-    
-   
-
-    #reseñas
-
-    # -------------------------------
-    # COMPRAS
-    # -------------------------------
-
-    # -------------------------------
-    # COMPRA VINILOS
-    # -------------------------------
-
-
-    # -------------------------------
-    # COMPRA CANCIONES
-    # -------------------------------
-
-
-    # -------------------------------
-    # CONTENIDO PLAYLIST
-    # -------------------------------
-    # -------------------------------
-    # CONTENIDO VINILOS
-    # -------------------------------
     return app
+
 
 app = create_app()
 
